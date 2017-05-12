d563914075e749354015cccf1533cf4281d43b34
Killfrra <Killfrra@users.noreply.github.com>
2017-05-12 16:42:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var stratum = require('stratum');
 var sha256 = require('fast-sha256');
+
 var Utils = {
 	stringToBytes: function (input) {
 		var bytes = new Uint8Array(input.length / 2);
@@ -13,6 +14,20 @@ var Utils = {
 			tmp = input[i].toString(16);
 			result += (tmp.length == 1)?('0'+tmp):tmp;
 		}
+		return result;
+	},
+	intToBytes: function (value) {
+		return new Uint8Array([(value & 0xff000000) >> 24, (value & 0x00ff0000) >> 16, (value & 0x0000ff00) >> 8, value & 0x000000ff]);
+	},
+	EndianFlip32BitChunks: function (input) {
+		//32 bits = 4 bytes = 8 chars?
+		var result = "";
+		for (let i = 0; i < input.length; i += 8)
+			for (let j = 0; j < 8; j += 2) {
+				//append byte (2 chars)
+				result += input[i - j + 6];
+				result += input[i - j + 7];
+			}
 		return result;
 	}
 }
@@ -39,7 +54,7 @@ client.on('mining.error', function(msg, socket){
 });
 //big-endian difficulty 1 target
 var block = {target:'00000000ffff0000000000000000000000000000000000000000000000000000'};
-var extranonce1,extranonce2_size,job_id,prevhash,coinb1,coinb2,merkle_branch,version,nbits,ntime,clean_jobs;
+var extranonce1,extranonce2 = 0,extranonce2_size,job_id,prevhash,coinb1,coinb2,merkle_branch,version,nbits,ntime,clean_jobs;
 
 // the client is a one-way communication, it receives data from the server after issuing commands
 client.on('mining', function(data, socket, type){
@@ -55,26 +70,28 @@ client.on('mining', function(data, socket, type){
   // client.subscription = the subscription data from the server
     if(data.method === 'set_difficulty'){
       block.target = (0x00000000ffff0000000000000000000000000000000000000000000000000000/data.params[0]).toString(16);
+      console.log('Setting new difficulty ',block.target);
     }else if(data.method === 'notify'){
-	job_id		= data.params[0] //Job ID. This is included when miners submit a results so work can be matched with proper transactions.
-	prevhash	= data.params[1] //Hash of previous block. Used to build the header.
-	coinb1		= data.params[2] //Coinbase (part 1). The miner inserts ExtraNonce1 and ExtraNonce2 after this section of the coinbase.
-	coinb2		= data.params[3] //Coinbase (part 2). The miner appends this after the first part of the coinbase and the two ExtraNonce values.
-	merkle_branch	= data.params[4] //List of merkle branches. The coinbase transaction is hashed against the merkle branches to build the final merkle root.
-	version		= data.params[5] //Bitcoin block version, used in the block header.
-	nbits		= data.params[6] //nBit, the encoded network difficulty. Used in the block header.
-	ntime		= data.params[7] //nTime, the current time. nTime rolling should be supported, but should not increase faster than actual time.
+	job_id		= data.params[0]
+	prevhash	= data.params[1]
+	coinb1		= data.params[2]
+	coinb2		= data.params[3]
+	merkle_branch	= data.params[4]
+	version		= data.params[5]
+	nbits		= data.params[6]
+	ntime		= data.params[7]
+	ntime_delta	= parseInt(ntime,16) - Math.floor(Date.now()/1000);
 	clean_jobs	= data.params[8] //Clean Jobs. If true, miners should abort their current work and immediately use the new job. If false, they can still use the current job, but should move to the new one after exhausting the current nonce 
-	var extranonce2 = job_id.padStart(extranonce2_size*2,'0');
-	var coinbase = coinb1 + extranonce1 + extranonce2 + coinb2;
+	extranonce2++;
+	var coinbase = coinb1 + extranonce1 + extranonce2.padStart(extranonce2_size*2,'0') + coinb2;
 	var coinbase_hash_bin = sha256(sha256(Utils.stringToBytes(coinbase)));
 	var merkle_root = new Uint8Array(64);
 	merkle_root.set(coinbase_hash_bin);
 	for(let i = 0; i < merkle_branch.length; i++){
-		merkle_root.set(Utils.stringToBytes(merkle_branch[i]),/*merkle_branch[i].length*/32);	//merkle_root = merkle_root.concat(merkle_branch[i])
-		merkle_root.set(sha256(sha256(merkle_root)));						//merkle_root = sha256d(merkle_root).concat(merkle_branch[i])
+		merkle_root.set(Utils.stringToBytes(merkle_branch[i]),32);
+		merkle_root.set(sha256(sha256(merkle_root)));
 	}
-	block.data = version + prevhash + Utils.bytesToString(merkle_root,32) + ntime + nbits + '00000000' + '000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000';
+	block.data = version + prevhash + Utils.EndianFlip32BitChunks(Utils.bytesToString(merkle_root,32)) + (Date.now()/1000 + ntime_delta).toString(16).padStart(8,'0') + nbits + '00000000' + '000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000';
 	console.log('new Block(\n'+JSON.stringify(block)+'\n)');
     }else if(data.result){
     	extranonce1 = data.result[1];
